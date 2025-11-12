1bf452a8409fa756f13228f474f0c2f72d6556dd
noahbustard <158092134+noahbustard@users.noreply.github.com>
2025-11-12 11:34:18 -0600

    Changes

--- Assignments/program00/main.cpp
+++ Assignments/program00/main.cpp
@@ -36,22 +36,26 @@ int main(int argc, char* argv[]) {
     SDL_RenderPresent(renderer);
 
     SDL_Event e;
-bool running = true;
-while (running) {
-    while (SDL_PollEvent(&e)) {
-        if (e.type == SDL_QUIT) {
-            running = false;
+    bool running = true;
+
+    Uint32 startTicks = SDL_GetTicks();
+    while (running) {
+        if (SDL_GetTicks() - startTicks >= 3000) {
+                SDL_DestroyRenderer(renderer);
+                SDL_DestroyWindow(window);
+                SDL_Quit();
+                running=false;
         }
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                running = false;
+            
+            }   
+        }
+        SDL_SetRenderDrawColor(renderer, 128, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16); // ~60 FPS throttle
     }
-
-    SDL_SetRenderDrawColor(renderer, 128, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(16); // ~60 FPS throttle
-}
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
     return 0;
 }
